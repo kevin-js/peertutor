265f2376199e988f006d0e624ed5460455fc3107
Kevin Guh <kevinjguh@yahoo.com>
2015-12-13 21:19:55 -0800

    added profile

--- website/app/views.py
+++ website/app/views.py
@@ -6,3 +6,12 @@ from flask import url_for, redirect, render_template
 @app.route('/index')
 def index():
 	return render_template('index.html', pagename="Home")
+
+# profile page
+@app.route('/profile/<username>')
+def profile(username):
+	# TODO: find user info in database
+	user_info = {
+		'name' : 'test'
+	}
+	return render_template('profile.html', pagename=username, user_info=user_info)
